48473f3f998478796687182fda4f6b04718be9d4
odedw <oded.welgreen@gmail.com>
2018-02-07 21:25:22 +0200

    improved nine-patch to work with multi line segments

--- View/ControllerView.js
+++ View/ControllerView.js
@@ -1,25 +1,9 @@
 const term = require('terminal-kit').terminal;
 const NinePatchAsciiDrawer = require('./NinePatchAsciiDrawer');
 const boxPatch = require('./box.json');
-const fader = `\n  ┌──────────────┐
-  │══════════════│
-  └──────────────┘`;
-const slot = `\n\n      ┌──────┐    
-      │      │    
-      │  ██  │    
-      │  ██  │    
-      │  ██  │    
-      │  ██  │    
-      │  ██  │    
-      │  ██  │    
-      │  ██  │    
-      │  ██  │    
-      │  ██  │    
-      │  ██  │    
-      │      │    
-      └──────┘    `;
-const lines = slot.split('\n').length - 2,
-  margin = 2;
+const faderHandlePatch = require('./fader-handle.json');
+const faderSlotPatch = require('./fader-slot.json');
+const margin = 2;
 
 class ControllerView {
   constructor() {
@@ -33,13 +17,15 @@ class ControllerView {
     term.clear();
     term.saveCursor();
     term.hideCursor();
-    this.ninePatchDrawer.draw(
-      boxPatch,
-      margin + 1,
-      margin + 1,
-      term.width - margin * 2,
-      term.height - margin * 2
-    );
+    // this.ninePatchDrawer.draw(
+    //   boxPatch,
+    //   margin + 1,
+    //   margin + 1,
+    //   term.width - margin * 2,
+    //   term.height - margin * 2
+    // );
+    // this.ninePatchDrawer.draw(faderHandlePatch, 5, 5, 15, 3);
+    // this.ninePatchDrawer.draw(faderSlotPatch, 5, 5, 6, 15);
 
     // const boxWidth = term.width - margin * 2 - 2;
     // for (let i = margin; i < term.height - margin; i++) {
--- View/NinePatchAsciiDrawer.js
+++ View/NinePatchAsciiDrawer.js
@@ -11,44 +11,61 @@ class NinePatchAsciiDrawer {
     // center segments fit in center sizes vert and horz
   }
 
-  getHeight(str) {
-    return str.split('\n').length;
-  }
-
-  drawRow(left, center, right, x, y, width) {
-    this.move(x, y);
-    this.d(left);
-    const repeats = (width - left.length - right.length) / center.length;
-    this.d(center.repeat(repeats));
-    this.d(right);
+  drawRow(left, center, right, x, y, w, h) {
+    const segmentHeight = left.length; //should be same for all segments
+    const repeats = (w - left[0].length - right[0].length) / center[0].length;
+    for (let i = 0; i < h; i++) {
+      this.move(x, y + i);
+      const row = i % segmentHeight;
+      this.d(left[row]);
+      this.d(center[row].repeat(repeats));
+      this.d(right[row]);
+    }
   }
 
   //currently each segment has to be 1x1
   draw(patch, x, y, width, height) {
     this.validate(patch);
+    const topHeight = patch.topLeft.length;
+    const bottomHeight = patch.bottomLeft.length;
+    const centerHeight = height - topHeight - bottomHeight;
     // top
-    this.drawRow(patch.topLeft, patch.topCenter, patch.topRight, x, y, width);
-
+    this.drawRow(
+      Array.isArray(patch.topLeft) ? patch.topLeft : [patch.topLeft],
+      Array.isArray(patch.topCenter) ? patch.topCenter : [patch.topCenter],
+      Array.isArray(patch.topRight) ? patch.topRight : [patch.topRight],
+      x,
+      y,
+      width,
+      topHeight
+    );
     //center
-    for (let i = 0; i < height - 2; i++) {
-      this.drawRow(
-        patch.centerLeft,
-        patch.centerCenter,
-        patch.centerRight,
-        x,
-        y + 1 + i,
-        width
-      );
-    }
-
+    this.drawRow(
+      Array.isArray(patch.centerLeft) ? patch.centerLeft : [patch.centerLeft],
+      Array.isArray(patch.centerCenter)
+        ? patch.centerCenter
+        : [patch.centerCenter],
+      Array.isArray(patch.centerRight)
+        ? patch.centerRight
+        : [patch.centerRight],
+      x,
+      y + topHeight,
+      width,
+      centerHeight
+    );
     //bottom
     this.drawRow(
-      patch.bottomLeft,
-      patch.bottomCenter,
-      patch.bottomRight,
+      Array.isArray(patch.bottomLeft) ? patch.bottomLeft : [patch.bottomLeft],
+      Array.isArray(patch.bottomCenter)
+        ? patch.bottomCenter
+        : [patch.bottomCenter],
+      Array.isArray(patch.bottomRight)
+        ? patch.bottomRight
+        : [patch.bottomRight],
       x,
-      y + height - 1,
-      width
+      y + height - bottomHeight,
+      width,
+      bottomHeight
     );
   }
 }
